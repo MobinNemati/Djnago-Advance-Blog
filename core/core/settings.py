c0325ba257db52f13276e684f44cf13dbddec3e5
mobin <6ix.mobin@gmail.com>
2024-08-05 23:09:52 +0330

    Custom User
    created custom User model, Post model and set setting in admin

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts',
-    'blog',
+    #'blog',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +125,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# user manager config
+
+AUTH_USER_MODEL = 'accounts.User'
