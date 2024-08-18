188b6a349d4632ba90f0d61829a3c0e7c9bab61e
mobin <6ix.mobin@gmail.com>
2024-08-18 19:24:07 +0330

    RedirectvVew
    test RedirectView and TemplateView

---
+++ core/blog/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+from django.views.generic.base import RedirectView
+
+
+app_name = 'blog'
+
+urlpatterns = [
+    #path('fbv-index', views.indexView, name='fbv-index'),
+    #path('cbv-index', TemplateView.as_view(template_name='index.html', extra_context={'name':'mobin'})),
+    #path('cbv-index', views.IndexView.as_view(), name='cbv-index'),
+    
+    # ba estefade az RedirectView mishe vaghti karbar vard in url mishe redirect beshe be link e ke dadim
+    path('go-to-maktabkhooneh', RedirectView.as_view(url='https://www.maktabkhooneh.com/'), name='go-to-maktabkhooneh'),
+    path('go-to-index', RedirectView.as_view(pattern_name='blog:cbv-index'), name='go-to-index'),
+
+    path('go-to-maktab/<int:pk>', views.RedirectToMaktab.as_view(), name='go-to-maktab'),
+
+]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from typing import Any
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, RedirectView
+from accounts.models import User
+from blog.models import Post
 
-# Create your views here.
+
+
+# ferestadan context ba function
+"""
+def indexView(request):
+    name = 'obin'
+    context = {'name':name}
+    return render(request, 'index.html', context)
+"""
+
+
+
+# ferestadan context ba class
+"""
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["name"] = 'mobin'
+        context['user'] = Post.objects.all()
+        return context
+"""
+
+
+
+# redirect kardan be maktabkhooneh ba estefade az function
+"""
+def RedirectToMaktab(requset):
+    return redirect('https://maktabkhooneh.com')
+"""
+
+
+# redirect kardan be maktabkhooneh ba estefade az class
+
+"""
+
+"""
+class RedirectToMaktab(RedirectView):
+    url = 'https://maktabkhooneh.com'
+
+    # def paeein pk e ke az url karbar vard karde ro migire mibine post e ba on pk hast ya na, age bod
+    # post ro print mikone dakhel terminal va user redirect mishe be url e ke neveshtim 
+    def get_redirect_url(self, *args, **kwargs):
+        post = get_object_or_404(Post, pk=kwargs['pk'])
+        print(post)
+        return super().get_redirect_url(*args, **kwargs)
